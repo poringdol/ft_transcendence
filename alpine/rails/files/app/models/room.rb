46dd090a34703a3f259d88bdaec54b5f9462a9a9
polinariabar <apollinaria.bar@gmail.com>
2021-03-25 15:44:01 +0300

    guild page js refactoring

--- alpine/rails/files/app/models/room.rb
+++
@@ -1,4 +0,0 @@
-class Room < ApplicationRecord
-  has_many :messages
-  has_many :users, through: :messages
-end
--- alpine/rails/files/test/models/guild_officer_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GuildOfficerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
