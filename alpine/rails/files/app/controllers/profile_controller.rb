c53d4f1305385a0a869f78c0f4167e0d535509bf
polinariabar <apollinaria.bar@gmail.com>
2021-03-27 22:45:45 +0300

    profile page

--- alpine/rails/files/app/controllers/profile_controller.rb
+++ alpine/rails/files/app/controllers/profile_controller.rb
@@ -1,3 +1,26 @@
 class ProfileController < ApplicationController
-	
+
+  def index
+	@user = current_user
+    respond_to do |format|
+      format.html { current_user }
+      format.json { render json: current_user}
+    end
+  end
+
+  def index_id
+    respond_to do |format|
+      format.html { 
+		  @user = User.find(params[:id])
+		  @user
+	   }
+      format.json { render json: @user}
+    end
+  end
+
+# Returns current user in JSON
+  def get_curr_user
+    render json: current_user
+  end
+
 end
--- copy_from_container.sh
+++ copy_from_container.sh
@@ -2,3 +2,5 @@
 docker cp alpine_rails_1:/pingpong/ ./
 sudo cp -rf pingpong/* alpine/rails/files/
 sudo rm -rf pingpong
+
+# docker cp alpine_rails_1:/pingpong/ ./alpine/rails/files
