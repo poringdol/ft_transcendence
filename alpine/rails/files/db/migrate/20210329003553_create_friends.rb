bd712f01ac5caf83759fcf41184e3f0162dde561
cddoma <poringdol@gmail.com>
2021-03-30 01:29:12 +0300

    fix frineds table

--- alpine/rails/files/db/migrate/20210329003553_create_friends.rb
+++ alpine/rails/files/db/migrate/20210329003553_create_friends.rb
@@ -1,8 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[6.0]
   def change
     create_table :friends do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :friend, null: false, foreign_key: true
+      t.references :user, index: true, foreign_key: { to_table: :users }
+      t.references :friend, index: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
