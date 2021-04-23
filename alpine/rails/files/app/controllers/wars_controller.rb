c9989a356c350a504749966e9716b6b54243d6eb
polinariabar <apollinaria.bar@gmail.com>
2021-04-23 23:31:20 +0300

    wars checks and form

--- alpine/rails/files/app/controllers/wars_controller.rb
+++ alpine/rails/files/app/controllers/wars_controller.rb
@@ -28,32 +28,44 @@ class WarsController < ApplicationController
 	puts('----------------------')
     unless guild_2
 	  respond_to do |format|
-        format.json { render json: { error: 'There is no such a guild'}, status: :unprocessable_entity}
+        format.json { render json: { error: 'There is no such a guild' }, status: :unprocessable_entity}
 	  end
-    else
-      date_start = params[:date_start]
-      time_start = params[:time_start]
-      war_start = DateTime.new(date_start.year, date_start.month, date_start.day, time_start.hour, time_start.min)
-
-      date_end = params[:date_end]
-      time_end = params[:time_end]
-      war_end = DateTime.new(date_end.year, date_end.month, date_end.day, time_end.hour, time_end.min)
-
-      if war_end - war_start < 0 || war_end - DateTime.now < 0
-        respond_to do |format|
-        	format.json { render json: { error: 'Dates are invalid'}, status: :unprocessable_entity}
-	 	end
-      end
-
-      @war = War.new(guild_1: current_user.guild_id, guild_2: guild_2, start: war_start, end: war_end, prize: params[:prize])
+	  return
+	end
+	unless current_user.guild_id
 	  respond_to do |format|
-		if @war.save
-			format.json { render json: @war, status: :created }
-		else
-			format.json { render json: @war.errors, status: :unprocessable_entity }
-		end
+		format.json { render json: { error: 'You are not in guild' }, status: :unprocessable_entity}
 	  end
-    end
+	  return
+	end
+	if current_user.guild_id == guild_2.id
+	  respond_to do |format|
+		format.json { render json: { error: 'You can not create match between the same guilds' }, status: :unprocessable_entity}
+	  end
+	  return
+	end
+	date_start = Date.parse params[:date_start]
+	time_start = Time.parse params[:time_start]
+	war_start = DateTime.new(date_start.year, date_start.month, date_start.day, time_start.hour, time_start.min)
+
+	date_end = Date.parse params[:date_end]
+	time_end = Time.parse params[:time_end]
+	war_end = DateTime.new(date_end.year, date_end.month, date_end.day, time_end.hour, time_end.min)
+
+	if war_end - war_start < 0 || war_end - DateTime.now < 0
+	  respond_to do |format|
+		format.json { render json: { error: 'Dates are invalid' }, status: :unprocessable_entity}
+	  end
+	end
+
+	@war = War.new(guild_1: current_user.guild, guild_2: guild_2, start: war_start, end: war_end, prize: params[:prize])
+	respond_to do |format|
+	  if @war.save
+		format.json { render json: @war, status: :created }
+	  else
+		format.json { render json: @war.errors, status: :unprocessable_entity }
+	  end
+	end
   end
 
   # PATCH/PUT /wars/1 or /wars/1.json
--- alpine/rails/files/app/serializers/war_serializer.rb
+++ alpine/rails/files/app/serializers/war_serializer.rb
@@ -2,5 +2,5 @@ class WarSerializer < ActiveModel::Serializer
   attributes :id, :start, :end, :prize, :max_unanswered, :guild_1_wins, :guild_2_wins
   has_one :guild_1
   has_one :guild_2
-  has_one :addon
+  has_one :addons
 end
